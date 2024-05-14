3e1cc61c23467d13a6da752729ee36c38703aecf
Zhamash Diar <zhamashdi@gmail.com>
2024-05-14 11:34:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Функция для отображения экрана профиля
+  function showProfileScreen() {
+    var profileMenu = createMenu(['Страница профиля', 'Уведомления', 'Закладки', 'Список друзей', 'Настройки']);
+    // код для отображения экрана профиля
+  }
+
+  // Ваш JavaScript код здесь
+});
